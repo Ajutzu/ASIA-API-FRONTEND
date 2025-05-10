8ce17bc96e1e80d4b6dc4bc7e3322ccc42de107d
AJ <aeronjamescastillo6@gmail.com>
2025-05-10 21:22:20 +0800

    update

--- components/home/Navbar.tsx
+++ components/home/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useEffect, useState } from "react"
 import { BarChart3, Moon, Sun } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { useTheme } from "next-themes"
